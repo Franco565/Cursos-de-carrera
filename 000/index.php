5acce09059d0e53504955eb7fefeb7690f916fc3
Franco565 <82671017+Franco565@users.noreply.github.com>
2021-09-22 13:56:47 -0500

    Add files via upload

---
+++ 000/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+   
+</head>
+<body>
+   <form action="validar.php" method="post">
+   <h1 class="animate_animated animate_backInLeft">Play Station</h1>
+   <p>Usuario <input type="text" placeholder="ingrese su nombre" name="usuario"></p>
+   <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
+    <input type="submit" value="Ingresar">
+    <a href="register.php">Registrarse</a>
+   
+   </form> 
+</body>
+</html>
